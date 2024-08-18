1abaf25aeac27409fc3ef973fba664f4076489b5
Moshood Kareem <42604147+Moshk94@users.noreply.github.com>
2024-08-18 02:00:47 +0100

    Add get random int to the list of helper Functions

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,30 +1,16 @@
+import { getRandomInt } from "./helperFunctions";
+
 let ctx  = document.getElementById('canvas').getContext("2d");
 
-canvas.addEventListener('mouseup', (e) => {
-  console.log('Do something with the left mouse button');
-});
+window.addEventListener('keyup', (e) => {
+  if (e.key === ' ') {
 
-window.addEventListener('keydown', (e) => {
-  if (e.key === 'ArrowRight') {
-    console.log('Do something with the right key');
-  }
-
-  if (e.key === 'ArrowLeft') {
-    console.log('Do something with the left key');
-  }
-
-  if (e.key === 'ArrowDown') {
-    console.log('Do something with the down key');
-  }
-
-  if (e.key === 'ArrowUp') {
-    console.log('Do something with the up key');
   }
 });
-
 
 animate();
 
 function animate() {
+  ctx.clearRect(0,0,canvas.width,canvas.height)
   requestAnimationFrame(animate);
 }
