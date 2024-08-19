4161c255a60917fe31643c1c484de10f53057495
Moshood Kareem <42604147+Moshk94@users.noreply.github.com>
2024-08-19 23:42:27 +0100

    Updated build script

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
+const cleanHTML = () => {
+  return {
+    name: "no-attribute",
+    transformIndexHtml(html) {
+      html = html.replace(`type="module"`, "")
+      html = html.replaceAll(`crossorigin`, "")
+      let scriptTag = html.match(/<script[^>]*>(.*?)<\/script[^>]*>/)[0]
+      html = html.replace(scriptTag, '')
+      html = html.replace("<!-- # INSERT SCRIPT HERE -->", scriptTag)
+      return html
+    }
+  }
+}
+
 module.exports = {
     root: 'dev',
     build: {
         outDir: '../dist',
-        emptyOutDir: true
+        emptyOutDir: true,
+        polyfillModulePreload: false
     },
-    base: './'
+    base: './',
+    plugins: [cleanHTML()]
 }
+
